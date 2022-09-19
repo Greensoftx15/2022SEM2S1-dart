41628d4bd30d576e54559ba2edbfe29afc506b0d
Greensoftx15 <rafael.manchego@unillanos.edu.co>
2022-09-18 22:10:35 -0500

    Creación de variales dart

--- 1.variables/variables.dart
+++ 1.variables/variables.dart
@@ -1,1 +1,10 @@
-void main() {}
+void main() {
+  var nombre = "Rafael";
+  String nombre2 = "Rafael";
+
+  final String nombre3 = 'Camilo';
+  const String nombre4 = 'David';
+
+  print(nombre2);
+  print(nombre4);
+}
